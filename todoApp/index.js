cc73463928bd336a49723f2a39c4e6817e03a5d0
changxianglin <281532612@qq.com>
2018-01-31 23:29:24 +0800

    easy to compration database

---
+++ todoApp/index.js
@@ -0,0 +1,19 @@
+// 引入模块
+const mongoose = require('mongoose')
+
+//链接数据库
+mongoose.connect('mongodb://todoapp:todoapp@ds121238.mlab.com:21238/todo')
+
+// 创建图表
+var todoSchema = new mongoose.Schema({
+    item: String
+})
+
+//往数据库中存储数据
+var Todo = mongoose.model('Todo', todoSchema)
+
+Todo({item: 'Hello Everyone!'}).save(function(err, data) {
+    if(err) throw err
+    console.log('Item saved')
+})
+
